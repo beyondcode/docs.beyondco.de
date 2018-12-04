bb58f5c5b36d7cff378d271c3d300f2687d5dcb1
Marcel Pociot <m.pociot@gmail.com>
2018-12-04 21:35:15 +0100

    wip

--- resources/docs/.vuepress/theme/WebSockets.vue
+++ resources/docs/.vuepress/theme/WebSockets.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="websocket-link can-hide" v-if="connected">
     <a class="">{{ userCount }} {{ users }} reading the documentation</a>
-    <a @click.prevent="notify" title="Notify other users"><div ref="bell" class="notification"></div></a>
+    <a @click.prevent="notify" v-tooltip="'Notify other users'"><div ref="bell" class="notification"></div></a>
   </div>
 </template>
 
@@ -26,6 +26,14 @@ export default {
   methods: {
     notify() {
       window.documentationChannel.whisper('notify');
+      this.ringBell();
+    },
+
+    ringBell() {
+      this.$refs.bell.classList.remove('notify');
+      setTimeout(() => {
+        this.$refs.bell.classList.toggle('notify');
+      } , 10);
     }
   },
 
@@ -44,10 +52,7 @@ export default {
 
       window.documentationChannel = Echo.join('documentation')
       .listenForWhisper('notify', (e) => {
-        this.$refs.bell.classList.remove('notify');
-        setTimeout(() => {
-          this.$refs.bell.classList.toggle('notify');
-        } , 10);
+        this.ringBell();
       })
       .here(users => {
         this.connected = true;
@@ -65,6 +70,111 @@ export default {
 </script>
 
 <style lang="css">
+.tooltip {
+  display: block !important;
+  z-index: 10000;
+}
+ 
+.tooltip .tooltip-inner {
+  background: black;
+  color: white;
+  border-radius: 16px;
+  padding: 5px 10px 4px;
+}
+ 
+.tooltip .tooltip-arrow {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  position: absolute;
+  margin: 5px;
+  border-color: black;
+  z-index: 1;
+}
+ 
+.tooltip[x-placement^="top"] {
+  margin-bottom: 5px;
+}
+ 
+.tooltip[x-placement^="top"] .tooltip-arrow {
+  border-width: 5px 5px 0 5px;
+  border-left-color: transparent !important;
+  border-right-color: transparent !important;
+  border-bottom-color: transparent !important;
+  bottom: -5px;
+  left: calc(50% - 5px);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+ 
+.tooltip[x-placement^="bottom"] {
+  margin-top: 5px;
+}
+ 
+.tooltip[x-placement^="bottom"] .tooltip-arrow {
+  border-width: 0 5px 5px 5px;
+  border-left-color: transparent !important;
+  border-right-color: transparent !important;
+  border-top-color: transparent !important;
+  top: -5px;
+  left: calc(50% - 5px);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+ 
+.tooltip[x-placement^="right"] {
+  margin-left: 5px;
+}
+ 
+.tooltip[x-placement^="right"] .tooltip-arrow {
+  border-width: 5px 5px 5px 0;
+  border-left-color: transparent !important;
+  border-top-color: transparent !important;
+  border-bottom-color: transparent !important;
+  left: -5px;
+  top: calc(50% - 5px);
+  margin-left: 0;
+  margin-right: 0;
+}
+ 
+.tooltip[x-placement^="left"] {
+  margin-right: 5px;
+}
+ 
+.tooltip[x-placement^="left"] .tooltip-arrow {
+  border-width: 5px 0 5px 5px;
+  border-top-color: transparent !important;
+  border-right-color: transparent !important;
+  border-bottom-color: transparent !important;
+  right: -5px;
+  top: calc(50% - 5px);
+  margin-left: 0;
+  margin-right: 0;
+}
+ 
+.tooltip.popover .popover-inner {
+  background: #f9f9f9;
+  color: black;
+  padding: 24px;
+  border-radius: 5px;
+  box-shadow: 0 5px 30px rgba(black, .1);
+}
+ 
+.tooltip.popover .popover-arrow {
+  border-color: #f9f9f9;
+}
+ 
+.tooltip[aria-hidden='true'] {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity .15s, visibility .15s;
+}
+ 
+.tooltip[aria-hidden='false'] {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity .15s;
+}
   .websocket-link {
     padding-left: 1em;
     padding-right: 1em;
